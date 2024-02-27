a85cca2026dc22a47229cdda7d2f7309b0ce761d
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2024-02-27 09:03:38 +0600

    fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import Navbar from "./components/Navbar";
 
 export default function App(){
     return (
-        <section className="h-screen bg-cover bg-Hero bg-center">
-          <Navbar />
+      <section className="h-screen bg-cover bg-Hero bg-center relative before:absolute before:inset-0 before:bg-black/35 z-10 before:z-20">
+          <div className="relative z-30">
+            <Navbar />
+          </div>
         </section>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,70 @@
+// import logo from "../assets/logo.png";
+// export default function Navbar() {
+//   return (
+//     <nav className="hover:bg-white">
+//       <div className="container mx-auto px-3 text-white">
+//         <div className="flex justify-between py-[21px]">
+//           <div className="grow-[0.5]">
+//             <img src={logo} alt="logo" />
+//           </div>
+//           <div>
+//             <ul className="flex flex-row space-x-[22px]">
+//                 <li>Living</li>
+//                 <li>Bedroom Furniture</li>
+//                 <li>Dining</li>
+//                 <li>Kitchen</li>
+//                 <li>Kids' Room</li>
+//                 <li>SmartFit</li>
+//                 <li>Institutional</li>
+//                 <li>Door</li>
+//                 <li>Interior</li>
+//                 <li>Office</li>
+//                 <li>More</li>
+//             </ul>
+//           </div>
+//           <div>
+//             <ul className="flex flex-row space-x-[17px]">
+//                 <li>Search</li>
+//                 <li>Cart</li>
+//                 <li>Profile</li>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+
 import logo from "../assets/logo.png";
 export default function Navbar() {
   return (
-    <nav className="hover:bg-white">
+    <nav className="hover:bg-white group">
       <div className="container mx-auto px-3">
         <div className="flex justify-between py-[21px]">
           <div className="grow-[0.5]">
             <img src={logo} alt="logo" />
           </div>
-          <div>
+          <div className="text-white font-400 group-hover:text-black">
             <ul className="flex flex-row space-x-[22px]">
-                <li>Living</li>
-                <li>Bedroom Furniture</li>
-                <li>Dining</li>
-                <li>Kitchen</li>
-                <li>Kids' Room</li>
-                <li>SmartFit</li>
-                <li>Institutional</li>
-                <li>Door</li>
-                <li>Interior</li>
-                <li>Office</li>
-                <li>More</li>
+              <li>Living</li>
+              <li>Bedroom Furniture</li>
+              <li>Dining</li>
+              <li>Kitchen</li>
+              <li>Kids' Room</li>
+              <li>SmartFit</li>
+              <li>Institutional</li>
+              <li>Door</li>
+              <li>Interior</li>
+              <li>Office</li>
+              <li>More</li>
             </ul>
           </div>
-          <div>
+          <div className="text-white group-hover:text-black">
             <ul className="flex flex-row space-x-[17px]">
-                <li>Search</li>
-                <li>Cart</li>
-                <li>Profile</li>
+              <li>Search</li>
+              <li>Cart</li>
+              <li>Profile</li>
             </ul>
           </div>
         </div>
